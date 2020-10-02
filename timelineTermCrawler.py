5b25b559b3077b33a497aa0e2c0b2331c2773996
João Pedro Israel <juanpeterisrael@gmail.com>
2020-10-02 18:10:00 -0300

    Versão 2, efeito Tatiana resolvido

--- timelineTermCrawler.py
+++ timelineTermCrawler.py
@@ -43,58 +43,70 @@ for userID in userList:
     continue
 
   # Caso não exista, criar o arquivo
-  else:
-    tweets = api.user_timeline(
-      screen_name=userList[0], 
-      # 200 é o valor máximo da conta
-      count=200,
-      include_rts = False,
-      # Extender os tweets além de 140 caractéres
-      tweet_mode = 'extended'
-      )
+  tweets = api.user_timeline(
+    screen_name=userID, 
+    # 200 é o valor máximo da conta
+    count=200,
+    include_rts = False,
+    # Extender os tweets além de 140 caractéres
+    tweet_mode = 'extended'
+    )
 
-    all_tweets = []
-    all_tweets.extend(tweets)
-    oldest_id = tweets[-1].id
-    tweet_term_list = []
+  all_tweets = []
+  all_tweets.extend(tweets)
+  oldest_id = tweets[-1].id
+  tweet_term_list = []
 
-    # if userID
-    while True:
-        tweets = api.user_timeline(
-            screen_name=userID, 
-            count=200,
-            include_rts = False,
-            max_id = oldest_id - 1,
-            tweet_mode = 'extended'
-          )
+  print(len(tweet_term_list))
+  # A tweet_term_list não está esvaziando
+  while True:
+      tweets = api.user_timeline(
+          screen_name=userID, 
+          count=200,
+          include_rts = False,
+          max_id = oldest_id - 1,
+          tweet_mode = 'extended'
+        )
 
-        # Quebrar o loop se acabarem os tweets
-        if len(tweets) == 0:
-          break
+      # Quebrar o loop se acabarem os tweets
+      if len(tweets) == 0:
+        break
 
-        oldest_id = tweets[-1].id
-        all_tweets.extend(tweets)
-        print(f'Numero de tweets analisados do perfil @{userID}: {len(all_tweets)}')
+      oldest_id = tweets[-1].id
+      all_tweets.extend(tweets)
+      print(f'Numero de tweets analisados do perfil @{userID}: {len(all_tweets)}')
 
-    # Pegar todos os tweets com os termos:
-    for tweet in all_tweets:
-      i = 0
-      while i < len(text_query):
-        # Se o tweet tiver os termos, adicionar
-        if tweet.full_text.find(text_query[i]) != -1:
-          tweet_term_list.append(tweet)
-        i += 1
+  # Pegar todos os tweets com os termos:
+  for tweet in all_tweets:
+    i = 0
+    while i < len(text_query):
+      # Se o tweet tiver os termos, adicionar
+      if tweet.full_text.find(text_query[i]) != -1:
+        tweet_term_list.append(tweet)
+      i += 1
 
-    print(f'Numero de tweets com o termos {text_query}: {len(tweet_term_list)}')
+  print(f'Numero de tweets com o termos {text_query}: {len(tweet_term_list)}')
 
-    outtweets = [
-      [tweet.id_str, 
-      tweet.created_at, 
-      tweet.favorite_count, 
-      tweet.retweet_count, 
-      tweet.full_text.encode("utf-8").decode("utf-8")] 
-      for idx,tweet in enumerate(tweet_term_list)
-      ]
-    df = DataFrame(outtweets,columns=["id","postado_em:","numero_de_favoritos","numero_de_retweets", "texto"])
-    df.to_csv(f'./basesDeDados/{date}/{area}/{userID}_tweets.csv',index=False)
-    df.head(3)
+  # Filtra tweets duplicados
+  # Sempre corta exatamente a metade dos tweets, algo parece errado
+  for tweet in tweet_term_list:
+    i = 0
+    while i < len(tweet_term_list):
+      # retornar a posição, se a posição existir, deve ser retirado
+      if tweet.id_str.find(tweet_term_list[i].id_str) != -1:
+        tweet_term_list.remove(tweet_term_list[i])
+      i += 1
+
+  print(f'Numero de tweets não duplicados: {len(tweet_term_list)}')
+
+  outtweets = [
+    [tweet.id_str, 
+    tweet.created_at, 
+    tweet.favorite_count, 
+    tweet.retweet_count, 
+    tweet.full_text.encode("utf-8").decode("utf-8")] 
+    for idx,tweet in enumerate(tweet_term_list)
+    ]
+  df = DataFrame(outtweets,columns=["id","postado_em:","numero_de_favoritos","numero_de_retweets", "texto"])
+  df.to_csv(f'./basesDeDados/{date}/{area}/{userID}_tweets.csv',index=False)
+  df.head(3)
