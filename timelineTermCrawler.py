26c5630a5e0e32b11ea016d6d3d15a0529c5eeab
João Pedro Israel <juanpeterisrael@gmail.com>
2020-10-03 13:37:27 -0300

    Pula para próxima conta na lista se a conta não tiver tweets

--- timelineTermCrawler.py
+++ timelineTermCrawler.py
@@ -16,14 +16,74 @@ area = 'universidades'
 
 # Lista de usuários
 userList = [
-  'unb_oficial', # (Universidade de Brasília)
-  'UFPBoficial', # (Universidade Federal da Paraíba)
-  'ufgdoficial', # (Universidade Federal da Grande Dourados)
-  'ufg_oficial', # (Universidade Federal de Goiás)
-  'UFMT',        # (Universidade Federal de Mato Grosso)'
-  'ufmsbr',       # (Universidade Federal de Mato Grosso do Sul)
-
+  'unb_oficial',    # (Universidade de Brasília)
+  'UFPBoficial',    # (Universidade Federal da Paraíba)
+  'ufgdoficial',    # (Universidade Federal da Grande Dourados)
+  'ufg_oficial',    # (Universidade Federal de Goiás)
+  'UFMT',           # (Universidade Federal de Mato Grosso)'
+  'ufmsbr',         # (Universidade Federal de Mato Grosso do Sul)
+  'ufcatoficial',   # (Universidade Federal de Catalão)'
+  'ufj_oficial',    # (Universidade Federal de Jataí)'
+  'ufbaempauta',    # (Universidade Federal da Bahia)'
+  'UFSB_OFICIAL',   # (Universidade Federal do Sul da Bahia)
+  'ufrb',           # (Universidade Federal do Recôncavo da Bahia)
+  'unilabbrasil',   # (Universidade Federal da Lusofonia Afro-Brasileira)
+  'UfalOficial',    # (Universidade Federal de Alagoas)
+  'UFCG',           # (Universidade Federal de Campina Grande)
+  'AscomUFPE',      # (Universidade Federal de Pernambuco)
+  'ufsoficial',     # (Universidade Federal de Sergipe)
+  'UFCinforma ',    # (Universidade Federal do Ceará)
+  'UFMAoficial',    # (Universidade Federal do Maranhão)
+  'ufpioficial',    # (Universidade Federal do Piauí)
+  'ufrnbr',         # (Universidade Federal do Rio Grande do Norte)
+  '_univasf',       # (Universidade Federal do Vale do São Francisco)
+  'UFRPEOnline',    #  (Universidade Federal Rural de Pernambuco)
+  'ufersa',         # (Universidade Federal Rural do Semi-Árido)
+  'ascomUNIR',      # (Universidade Federal de Rondônia)
+  'UFRROficial',    # (Universidade Federal de Roraima)
+  'ufac_oficial',   # (Universidade Federal do Acre)
+  'unifapoficial',  # (Universidade Federal do Amapá)
+  'UFAM_',          # (Universidade Federal do Amazonas)
+  'ufopa',          # (Universidade Federal do Oeste do Pará)
+  'UFPA_Oficial',   # (Universidade Federal do Pará)
+  'UFToficial',     # (Universidade Federal do Tocantins)
+  'UfraOficial',    # (Universidade Federal Rural da Amazônia)
+  'unifesspa',      # (Universidade Federal do Sul e Sudeste do Pará)
+  'unifalmgOFICIAL',# (Universidade Federal de Alfenas)
+  'unifei_oficial', # (Universidade Federal de Itajubá)
+  'UFJF_',          # (Universidade Federal de Juiz de Fora)
+  'uflabr',         # (Universidade Federal de Lavras)
+  'ufmg',           # (Universidade Federal de Minas Gerais)
+  'UFOP',           # (Universidade Federal de Ouro Preto)
+  'ufscaroficial',  # (Universidade Federal de São Carlos)
+  'ufsjbr',         # (Universidade Federal de São João del-Rei)
+  'unifesp',        # (Universidade Federal de São Paulo)
+  'UFU_Oficial',    # (Universidade Federal de Uberlândia)
+  'ufvbr',          # (Universidade Federal de Viçosa)
+  'ufabc',          # (Universidade Federal do ABC)
+  'ufesoficial',    # (Universidade Federal do Espírito Santo)
+  'UNIRIO_Oficial', # (Universidade Federal do Estado do Rio de Janeiro)
+  'ufrj',           # (Universidade Federal do Rio de Janeiro)
+  'uftmsocial',     # (Universidade Federal do Triângulo Mineiro)
+  'uftmsocial',     # (Universidade Federal dos Vales do Jequitinhonha e Mucuri)
+  'uff_br',         # (Universidade Federal Fluminense)
+  'ufrrjbr',        # (Universidade Federal Rural do Rio de Janeiro)
+  'UTFPR_',         # (Universidade Tecnológica Federal do Paraná)
+  'uffsonline',     # (Universidade Federal da Fronteira Sul)
+  'unila',          # (Universidade Federal da Integração Latino-Americana)
+  'UFCSPA',         # (Universidade Federal de Ciências da Saúde de Porto Alegre)
+  'UFPel',          # (Universidade Federal de Pelotas)
+  'UFSC',           # (Universidade Federal de Santa Catarina)
+  'UFSM_oficial',   # (Universidade Federal de Santa Maria)
+  'UnipampaOficial',# (Universidade Federal do Pampa)
+  'UFPR',           # (Universidade Federal do Paraná)
+  'FURG',           # (Universidade Federal do Rio Grande)
+  'ufrgsnoticias',  # (Universidade Federal do Rio Grande do Sul)
+  'UFAPE_oficial',  # (Universidade Federal do Agreste de Pernambuco)
+  'UFDPar',         # (Universidade Federal do Delta do Parnaíba)
+  'UFToficial',     # (Universidade Federal do Norte do Tocantins)
   ]
+
 # Lista de termos
 text_query = ['covid', 'pandemia', 'sars', 'corona']
 
@@ -54,7 +114,14 @@ for userID in userList:
 
   all_tweets = []
   all_tweets.extend(tweets)
-  oldest_id = tweets[-1].id
+
+  # Caso o perfil não possua tweets, pular para a próxima conta
+  if len(tweets) != 0:
+    oldest_id = tweets[-1].id
+  else:
+    print(f"O perfil @{userID} não possui nenhum tweet")
+    continue
+
   tweet_term_list = []
 
   # A tweet_term_list não está esvaziando
@@ -95,7 +162,6 @@ for userID in userList:
       i += 1
 
   print(f'Numero de tweets com o termos {text_query}: {len(tweet_term_list)}')
-  print(f'Numero de tweets não duplicados: {len(tweet_term_list)}')
 
   outtweets = [
     [tweet.id_str, 
